c3a76722fcc84ef7fe97d8b356a316233c452a54
yusivacova <sivacovayulia@gmail.com>
2023-08-11 10:36:57 +0300

    feat: add styles for 404 page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,7 +1,9 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
-import { Box, ThemeProvider, createTheme } from '@mui/material';
+import { Box, ThemeProvider, createTheme, Typography } from '@mui/material';
 import { themeOptions } from '../../assets/theme1';
+import { Link } from 'react-router-dom';
+import NotFoundImage from './not-found-img';
 
 const plantsTheme = createTheme(themeOptions);
 
@@ -28,5 +30,38 @@ export function NotFoundPage() {
 }
 
 function ContentNotFoundPage() {
-  return <div>I am error 404</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: plantsTheme.palette.text.primary,
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant="h2" component="h1" gutterBottom>
+          <p>Oops!</p>
+          <p>The page you requested was not found.</p>
+        </Typography>
+        <NotFoundImage />
+        <Box
+          sx={{
+            fontSize: '24px',
+            marginTop: '40px',
+            py: '10px',
+            px: '10px',
+            background: plantsTheme.palette.background.default,
+            borderRadius: '10%',
+            color: plantsTheme.palette.text.primary,
+          }}
+        >
+          <Link to="/">Home</Link>
+        </Box>
+      </Box>
+    </>
+  );
 }
