d9c2e66559efea379d100fda58f67e2e6a0d7c5e
yusivacova <sivacovayulia@gmail.com>
2023-08-12 17:47:00 +0300

    feat: add login link for register page

--- src/pages/registration/registration_page.tsx
+++ src/pages/registration/registration_page.tsx
@@ -1,5 +1,6 @@
 import { Box, createTheme, Typography } from '@mui/material';
 import { themeOptions } from '../../assets/theme1';
+import { Link } from 'react-router-dom';
 
 const plantsTheme = createTheme(themeOptions);
 
@@ -20,6 +21,29 @@ export function RegistrationPage() {
         <Typography variant="h2" component="h1" gutterBottom>
           Registration Page
         </Typography>
+        <LoginLink />
+      </Box>
+    </>
+  );
+}
+
+function LoginLink() {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '8px',
+        }}
+      >
+        Already have an account?
+        <Box
+          sx={{
+            textDecoration: 'underline',
+          }}
+        >
+          <Link to="/login"> Log in</Link>
+        </Box>
       </Box>
     </>
   );
