24c3ea42ae7f600987cc9f8cf54910066ba5bcd3
yusivacova <sivacovayulia@gmail.com>
2023-08-14 22:55:08 +0300

    feat: add guarantee page

--- src/pages/guarantee/guarantee_page.tsx
+++ src/pages/guarantee/guarantee_page.tsx
@@ -1,6 +1,8 @@
 import { Box, createTheme, Typography } from '@mui/material';
 import { themeOptions } from '../../assets/theme1';
 import { PAGES_TITLES } from '../../data/TITLES';
+import { TEXT_GUARANTEE } from './dataGuarantee';
+import GuaranteeImage from './guarantee-img';
 
 const plantsTheme = createTheme(themeOptions);
 
@@ -13,15 +15,52 @@ export function GuaranteePage() {
           flexDirection: 'column',
           width: '100%',
           justifyContent: 'center',
+          alignItems: 'center',
+          padding: '15px',
+          color: plantsTheme.palette.text.primary,
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          {PAGES_TITLES.guarantee}
+        </Typography>
+        <GuaranteeContent />
+      </Box>
+    </>
+  );
+}
+
+export function GuaranteeContent() {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
           alignItems: 'flex-start',
           padding: '15px',
           color: plantsTheme.palette.text.primary,
         }}
       >
-        <Typography variant="h2" component="h1" gutterBottom>
-          {PAGES_TITLES.guarantee}
-        </Typography>
+        <GuaranteeImage />
+        <TextGuarantee />
       </Box>
     </>
   );
+}
+
+function TextGuarantee() {
+  const text = TEXT_GUARANTEE.map((item, index) => (
+    <Typography
+      key={index + 'text-guarantee'}
+      variant="body2"
+      color="text.secondary"
+      gutterBottom
+      sx={{ textAlign: 'justify' }}
+    >
+      {item}
+    </Typography>
+  ));
+
+  return <Box sx={{ margin: '0 40px' }}>{text}</Box>;
 }
