86bce67f42b5e5a6ccc2c04b09b97b6b5fb84d5a
pinkpony1106 <juliaegorova221098@gmail.com>
2023-08-20 23:48:56 +0300

    feat: add login form

--- src/pages/login/login_page.tsx
+++ src/pages/login/login_page.tsx
@@ -1,14 +1,73 @@
-import { Box, createTheme, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, createTheme, Typography, TextField, FormGroup } from '@mui/material';
 import Button from '@mui/material/Button';
 
 import { themeOptions } from '../../assets/theme1';
 import { Link } from 'react-router-dom';
 import { PagesTitles } from '../../data/TITLES';
 import { PATH } from '../../data/PATH';
+import { DetailsType } from './login.types';
 
 const plantsTheme = createTheme(themeOptions);
 
 export function LoginPage() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
+
+  function encodeLoginRequestBody() {
+    const details: DetailsType = {
+      grant_type: 'password',
+      username: email,
+      password: password,
+    };
+
+    const formBody: string[] = [];
+    for (let property in details) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(details[property as keyof typeof details]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    return formBody.join('&');
+  }
+
+  async function loginUser() {
+    const response = await fetch(
+      'https://auth.europe-west1.gcp.commercetools.com/oauth/ecommerceapp_951/customers/token',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization:
+            'Basic N29zbFJYLVN3dUVPWWsycERSeHdTSS1wOmRXX0Q1eGNRU004WC1lbHRxUXFyMHp1bVRQUGo4QVlt',
+        },
+        body: encodeLoginRequestBody(),
+      }
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    loginUser().then((response) => {
+      if (response.statusCode === 400) {
+        alert(response.message);
+      } else if (response.access_token) {
+        navigate(PATH.main);
+      }
+    });
+  };
+
+  //   const validateEmail = (mail: string) => {
+  //     return String(mail)
+  //       .toLowerCase()
+  //       .match(
+  //         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //       );
+  //   };
+
   return (
     <>
       <Box
@@ -25,6 +84,35 @@ export function LoginPage() {
         <Typography variant="h2" component="h1" gutterBottom>
           {PagesTitles.login}
         </Typography>
+        <form>
+          <FormGroup
+            sx={{
+              gap: '15px',
+              marginBottom: '50px',
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(event.target.value)
+              }
+            />
+            <TextField
+              id="outlined-basic"
+              type="password"
+              label="Password"
+              variant="outlined"
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(event.target.value)
+              }
+            />
+            <Button variant="contained" onClick={(e) => handleLogin(e)}>
+              Login
+            </Button>
+          </FormGroup>
+        </form>
         <RegisterLink />
       </Box>
     </>
