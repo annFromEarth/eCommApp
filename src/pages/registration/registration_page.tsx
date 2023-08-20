3193f3e1ce6a0baf753a75adb39b90c5dd1b6146
annFromEarth <dv_anna@mail.ru>
2023-08-20 17:12:59 +0300

    refactor: change register page layout

--- src/pages/registration/registration_page.tsx
+++ src/pages/registration/registration_page.tsx
@@ -1,10 +1,27 @@
-import { Box } from '@mui/material';
 import Form1 from '../../components/registration-form/registration-form';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+import { Box, ThemeProvider, createTheme } from '@mui/material';
+import { themeOptions } from '../../assets/theme1';
+
+const plantsTheme = createTheme(themeOptions);
 
 export function RegistrationPage() {
   return (
-    <Box>
-      <Form1 />
-    </Box>
+    <ThemeProvider theme={plantsTheme}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          background: plantsTheme.palette.background.paper,
+          backgroundSize: 'cover',
+        }}
+      >
+        <Header />
+        <Form1 />
+        <Footer />
+      </Box>
+    </ThemeProvider>
   );
 }
