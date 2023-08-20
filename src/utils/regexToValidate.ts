4c844a3f0f509a78c5d5e3522c99f4d435efff44
annFromEarth <dv_anna@mail.ru>
2023-08-20 17:09:02 +0300

    feat: add regex for form validation

---
+++ src/utils/regexToValidate.ts
@@ -0,0 +1,12 @@
+export const emailRegExpRFC =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+export const nameRegExp = /^[a-z ,.'-]+$/i;
+
+export const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+
+export const postcodeRegEx = /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/;
+
+export const streetRegEx = /^[#.0-9a-zA-Z\s,-]+$/;
+
+export const cityRegEx = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
