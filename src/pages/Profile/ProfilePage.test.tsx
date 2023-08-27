55d3cb5d5efd66a5f66850eff4228dc42352ac52
yusivacova <sivacovayulia@gmail.com>
2023-08-27 23:45:01 +0300

    fix: fix name test

--- src/pages/Profile/ProfilePage.test.tsx
+++ src/pages/Profile/ProfilePage.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
 import { ProfilePage } from './ProfilePage';
 
-test('should be rendered Shipping page', () => {
+test('should be rendered Profile page', () => {
   render(
     <BrowserRouter>
       <ProfilePage />
