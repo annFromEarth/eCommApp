ecaf80744a9a013ba4a0c9e69bd684bafc222ac1
annFromEarth <dv_anna@mail.ru>
2023-08-27 21:47:13 +0300

    refactor: change registration form to combine @mui and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import Footer from './components/Footer/Footer';
 import RoutingApp from './services/routing/routing';
 import { Box, ThemeProvider, createTheme } from '@mui/material';
 import { themeOptions } from './assets/theme';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const plantsTheme = createTheme(themeOptions);
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={plantsTheme}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100vh',
-            background: plantsTheme.palette.background.paper,
-            backgroundSize: 'cover',
-          }}
-        >
-          <Header />
-          <RoutingApp />
-          <Footer />
-        </Box>
-      </ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeProvider theme={plantsTheme}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              minHeight: '100vh',
+              background: plantsTheme.palette.background.paper,
+              backgroundSize: 'cover',
+            }}
+          >
+            <Header />
+            <RoutingApp />
+            <Footer />
+          </Box>
+        </ThemeProvider>
+      </LocalizationProvider>
     </>
   );
 }
--- src/components/registrationForm/validationConstants.ts
+++ src/components/registrationForm/validationConstants.ts
@@ -8,6 +8,9 @@ import {
 } from '../../utils/regexToValidate';
 
 export const PATTERNS = {
+  EMPTY: {
+    message: 'This field is required',
+  },
   FIRST_NAME: {
     value: nameRegExp,
     message: 'At least one letter, no numbers, no special characters',
@@ -20,17 +23,14 @@ export const PATTERNS = {
   PASSWORD: {
     value: passwordRegExp,
     message:
-      'Password too weak. Please use minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character',
+      'Password too weak! Use minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character',
   },
   POSTCODE: {
     value: postcodeRegEx,
     message:
       'Five to seven alphanumeric (uppercase) characters separated by a space. Example: "AA1 1AA" or "AA11 1AA"',
   },
-  STREET: {
-    value: streetRegEx,
-    message: 'Not a valid street address',
-  },
+  STREET: { value: streetRegEx, message: 'Not a valid street address' },
   CITY: {
     value: cityRegEx,
     message: 'Not a valid city name',
