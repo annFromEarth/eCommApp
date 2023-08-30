e835113b07412ee70092847bae8bec18fec39d03
pinkpony1106 <juliaegorova221098@gmail.com>
2023-08-30 20:13:28 +0300

    feat: added layout for cards, discount prices

--- src/components/Catalog/catalogRequest.ts
+++ src/components/Catalog/catalogRequest.ts
@@ -2,9 +2,7 @@ export async function getProducts() {
   const authorizationToken: string = window.sessionStorage.getItem('authorization-token')!;
 
   const response = await fetch(
-    `${import.meta.env.VITE_CLIENT_CTP_API_URL}/${
-      import.meta.env.VITE_CLIENT_CTP_PROJECT_KEY
-    }/product-projections`,
+    `${process.env.VITE_CLIENT_CTP_API_URL}/${process.env.VITE_CLIENT_CTP_PROJECT_KEY}/product-projections`,
     {
       method: 'GET',
       headers: {
