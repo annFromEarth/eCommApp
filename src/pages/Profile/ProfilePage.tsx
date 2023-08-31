6c5fc62deb907121ed1adb7b64bdb40f4b76fbd8
annFromEarth <dv_anna@mail.ru>
2023-08-31 22:28:33 +0300

    feat: add customer info representation and edit mode switch to user profile page

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,21 +1,63 @@
-import { Box, useTheme, Typography } from '@mui/material';
+import { Box, useTheme, Typography, Button } from '@mui/material';
 import { PAGES_TITLES } from '../../data/titles';
 import { PATH } from '../../data/paths';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// import getCustomer from '../../services/getCustomer';
+import './profilePage.css';
+import { Customer } from './types';
+import { CustomerService } from '../../services/customerService';
 
 export function ProfilePage() {
   const plantsTheme = useTheme();
   const navigate = useNavigate();
-  //   const customerData = getCustomer();
-  //   console.log(customerData);
+
+  const [customerData, setCustomerData] = useState<Customer | null>(null);
+
+  const [editPersonalData, setEditPersonalData] = useState<boolean>(false);
+  const [editPassword, setEditPassword] = useState<boolean>(false);
+  const [editAddresses, setEditAddresses] = useState<boolean>(false);
+
+  const toggleEditPersonalData = () => {
+    setEditPersonalData((editMode) => !editMode);
+  };
+
+  const toggleEditPassword = () => {
+    setEditPassword((editMode) => !editMode);
+  };
+
+  const toggleEditAddresses = () => {
+    setEditAddresses((editMode) => !editMode);
+  };
+
+  const fetchCustomerData = async (token: string) => {
+    //Try/catch
+    const customer = await CustomerService.getMe(token);
+    setCustomerData(customer);
+  };
+
+  const arrayAddresses = customerData?.addresses.map((address, index) => (
+    <li className="addressItem" key={address.id}>
+      <p>Address {index + 1}</p>
+      <p>Country: {address.country}</p>
+      <p>Postal Code: {address.postalCode}</p>
+      <p>City: {address.city}</p>
+      <p>Street: {address.streetName}</p>
+      {customerData.defaultShippingAddressId === address.id ? (
+        <p>✅ Default shipping address</p>
+      ) : (
+        ''
+      )}
+      {customerData.defaultBillingAddressId === address.id ? <p>✅ Default billing address</p> : ''}
+    </li>
+  ));
 
   useEffect(() => {
-    if (!sessionStorage.getItem('authorization-token')) {
+    const authorizationToken: string = sessionStorage.getItem('authorization-token')!;
+    if (!authorizationToken) {
       navigate(PATH.login);
     }
+    fetchCustomerData(authorizationToken);
   }, [navigate]);
 
   return (
@@ -34,16 +76,44 @@ export function ProfilePage() {
         <Typography variant="h2" component="h1" gutterBottom>
           {PAGES_TITLES.profile}
         </Typography>
-        <Box>
-          <Typography variant="h6" component="h6" gutterBottom>
-            Personal information
+        <Box className="container">
+          <Typography variant="h5" component="h5" gutterBottom>
+            Personal Data
           </Typography>
-          <Typography variant="h6" component="h6" gutterBottom>
-            Password
-          </Typography>
-          <Typography variant="h6" component="h6" gutterBottom>
+          <Button variant="contained" onClick={toggleEditPersonalData}>
+            {!editPersonalData && 'edit'}
+            {editPersonalData && 'return'}
+          </Button>
+          {!editPersonalData && (
+            <>
+              <p>First Name: {customerData ? customerData.firstName : ''}</p>
+              <p>Last Name: {customerData ? customerData.lastName : ''}</p>
+              <p>Date of Birth: {customerData ? customerData.dateOfBirth : ''}</p>
+              <p>Email: {customerData ? customerData.email : ''}</p>
+            </>
+          )}
+          {editPersonalData && <>Editing</>}
+        </Box>
+        <Box className="container">
+          <Typography variant="h5" component="h5" gutterBottom>
             Addresses
           </Typography>
+          <Button variant="contained" onClick={toggleEditAddresses}>
+            {!editAddresses && 'edit'}
+            {editAddresses && 'return'}
+          </Button>
+          {!editAddresses && <ul>{arrayAddresses}</ul>}
+          {editAddresses && <>Editing</>}
+        </Box>
+        <Box className="container">
+          <Typography variant="h5" component="h5" gutterBottom>
+            Password
+          </Typography>
+          <Button variant="contained" onClick={toggleEditPassword}>
+            {!editPassword && 'change password'}
+            {editPassword && 'return'}
+          </Button>
+          {editPassword && <>Editing</>}
         </Box>
       </Box>
     </>
